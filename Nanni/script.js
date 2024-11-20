93db2d8140c287cdddcb4f8c33db1b889a839d1a
Luiz Gabriel F. Martins <94010799+LuFMartins@users.noreply.github.com>
2024-11-20 15:53:59 -0300

    Ajustando código
    Ajustando código e adicionando a tela de cadastro de jogos

---
+++ Nanni/script.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCeM1rCRrx9_VEayGBO0aas946aOZNAnaQ",
+  authDomain: "nannitest-2ad96.firebaseapp.com",
+  projectId: "nannitest-2ad96",
+  storageBucket: "nannitest-2ad96.firebasestorage.app",
+  messagingSenderId: "330907496083",
+  appId: "1:330907496083:web:f711c4e666d9247ab0a766"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
